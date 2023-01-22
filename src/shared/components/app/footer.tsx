b8283a857c97299b22fdd9f16aa8d2e30c8516fc
sarintoxic <nguyentrunghieux24@gmail.com>
2023-01-22 17:12:38 +0300

    Footer updated
    Add new footer

--- src/shared/components/app/footer.tsx
+++ src/shared/components/app/footer.tsx
@@ -1,7 +1,5 @@
 import { Component } from "inferno";
 import { GetSiteResponse } from "lemmy-js-client";
-import { i18n } from "../../i18next";
-import { joinLemmyUrl, joinPiUrl, repoUrl } from "../../utils";
 interface FooterProps {
   site: GetSiteResponse;
 }
@@ -13,60 +11,64 @@ export class Footer extends Component<FooterProps, any> {
 
   render() {
     return (
-      <nav className="container-lg navbar navbar-expand-md navbar-light navbar-bg p-3">
-        <div className="navbar-collapse">
-          <ul className="navbar-nav ml-auto">
-            {/* {this.props.site.version !== VERSION && (
-              <li class="nav-item">
-                <span class="nav-link">UI: {VERSION}</span>
-              </li>
-            )}
-            <li className="nav-item">
-              <span className="nav-link">BE: {this.props.site.version}</span>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/modlog">
-                {i18n.t("modlog")}
-              </NavLink>
-            </li>
-            {this.props.site.site_view.local_site.legal_information && (
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/legal">
-                  {i18n.t("legal_information")}
-                </NavLink>
-              </li>
-            )}
-            {this.props.site.federated_instances && (
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/instances">
-                  {i18n.t("instances")}
-                </NavLink>
-              </li>
-            )}
-            <li className="nav-item">
-              <a className="nav-link" href={docsUrl}>
-                {i18n.t("docs")}
-              </a>
-            </li>
-	    */}
-            <li className="nav-item">
-              <a className="nav-link" href={repoUrl}>
-                {i18n.t("code")}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={joinLemmyUrl}>
-                Fork from Lemmy
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href={joinPiUrl}>
-                Join Pi Network
-              </a>
-            </li>
-          </ul>
+      <footer className="footer-distributed">
+        <div className="footer-left">
+          <h3>
+            We<span>Pi</span>
+          </h3>
+
+          <p className="footer-links">
+            <a href="#" className="link-1">
+              Copyright © 2023
+            </a>
+          </p>
+
+          <p className="footer-company-name">Copyright © 2023</p>
         </div>
-      </nav>
+
+        <div className="footer-center">
+          <div>
+            <i className="fa fa-map-marker"></i>
+            <p>
+              <span>Tòa nhà 69</span> Quận Bình Chánh, Hà Nội
+            </p>
+          </div>
+
+          <div>
+            <i className="fa fa-phone"></i>
+            <p>+84.555.555.555</p>
+          </div>
+
+          <div>
+            <i className="fa fa-envelope"></i>
+            <p>
+              <a href="mailto:contact@wepi.social">contact@wepi.social</a>
+            </p>
+          </div>
+        </div>
+
+        <div className="footer-right">
+          <p className="footer-company-about">
+            <span>Mạng xã hội WePi</span>
+            Trực thuộc và quản lý bởi CK Capital.
+          </p>
+
+          <div className="footer-icons">
+            <a href="#">
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
     );
   }
 }
