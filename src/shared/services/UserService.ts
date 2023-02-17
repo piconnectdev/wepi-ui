714f25f8a0c431da9e8e70a1c2ceb54a8dfe49d5
Pi Connect Dev <piconnectdev@gmail.com>
2023-02-17 13:02:20 +0700

    js-cookie

--- src/shared/services/UserService.ts
+++ src/shared/services/UserService.ts
@@ -1,5 +1,6 @@
 // import Cookies from 'js-cookie';
 import IsomorphicCookie from "isomorphic-cookie";
+import { Cookies } from "js-cookie";
 import jwt_decode from "jwt-decode";
 import { LoginResponse, MyUserInfo } from "lemmy-js-client";
 import { BehaviorSubject } from "rxjs";
@@ -38,7 +39,13 @@ export class UserService {
     expires.setDate(expires.getDate() + 365);
     if (res.jwt) {
       toast(i18n.t("logged_in"));
+      console.log("saveJwtInfo:" + res.jwt);
       IsomorphicCookie.save("jwt", res.jwt, { expires, secure: isHttps });
+      Cookies.set("wepiJwt", res.jwt, {
+        expires,
+        domain: location.host,
+        secure: isHttps,
+      });
       this.setJwtInfo();
     }
   }
@@ -47,7 +54,9 @@ export class UserService {
     this.jwtInfo = undefined;
     this.myUserInfo = undefined;
     IsomorphicCookie.remove("jwt"); // TODO is sometimes unreliable for some reason
-    document.cookie = "jwt=; Max-Age=0; path=/; domain=" + location.hostname;
+    Cookies.remove("wepiJwt");
+    document.cookie =
+      "jwt=; Max-Age=0; path=/; wepiJwt=; domain=" + location.hostname;
     location.reload();
   }
 
@@ -71,6 +80,10 @@ export class UserService {
 
     if (jwt) {
       this.jwtInfo = { jwt, claims: jwt_decode(jwt) };
+    } else {
+      jwt = Cookies.get("wepiJwt");
+      console.log("setJwtInfo from Cookies");
+      if (jwt) this.jwtInfo = { jwt, claims: jwt_decode(jwt) };
     }
     console.log("setJwtInfo:" + JSON.stringify(this.jwtInfo));
   }
